1a1502d1a9c4fca841baeeec547876fd9f1399a3
Colaborador jose <joselgomezc1@gmail.com>
2021-10-25 10:13:41 -0500

    webpack.config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-pluggin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-    resolve: {
-        extensions:['.js']
-    },
-    module: 
-    {
-    rules: 
-    [
-        {
-            test:/\.m?js$/,
-            exclude:/node_modules/,
-            use:{
-                loader: 'babel-loader'
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
+      }
     ]
-
-
-    },
-    // SECCION DE PLUGINS
-    plugins: [
-        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
-            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
-            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
-        })
-    ]
-    
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
 }
